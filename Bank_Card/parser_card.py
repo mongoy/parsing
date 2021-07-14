2a14b717c607a2b2edabede5b694d0fda830ef90
mongoy <mongoy@rambler.ru>
2021-07-14 11:53:27 +0900

    Rebuilde parser_card script

--- Bank_Card/parser_card.py
+++ Bank_Card/parser_card.py
@@ -30,52 +30,50 @@ def get_html(url, params=''):
     return response
 
 
-# print(get_html(URL, ''))
 def get_content(html):
+    """
+    Faind all tags with attribute class = 'sc-14ydfjo-0 jYdA-De' - card description
+    :param html:
+    :return:
+    """
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('div', class_="sc-14ydfjo-0 jYdA-De")
     cards_list = []
     for count, item in enumerate(items):
-        print(count, item)
-        cards = item.find_all('div', class_="bfjox4-9 blqfcL")
-        for index, card in enumerate(cards):
-            print(count, index, item)
-            tag_name = card.find('a', class_="sc-6nr3q5-0 iyqRre")  # name card
+        """
+        list cards
+        """
+        tag_name = item.find('a', class_="sc-6nr3q5-0 iyqRre")  # name card
 
-            if tag_name is not None:
-                tag_linc = HOST + card.find('a', class_="sc-6nr3q5-0 iyqRre")['href'][1:]  #
+        if tag_name is not None:
+            tag_linc = HOST + item.find('a', class_="sc-6nr3q5-0 iyqRre")['href'][1:]  #
 
-            else:
-                tag_name = card.find('div', class_="sc-6nr3q5-0 iyqRre")
-                tag_linc = '-'
+        else:
+            tag_name = item.find('div', class_="sc-6nr3q5-0 iyqRre")
+            tag_linc = '-'
 
-            tag_bank = card.find('span', class_="bfjox4-20 jtOFL")  # bank
-            tag_img = card.find('img', class_="bfjox4-10 ekxCIS")['src']  # image
+        tag_bank = item.find('span', class_="bfjox4-20 jtOFL")  # bank
+        tag_img = item.find('img', class_="bfjox4-10 ekxCIS")['src']  # image
 
-            cards_list.append(
-                {
-                    'title': tag_name.text.strip(),
-                    'bank': tag_bank.text.strip(),
-                    'link': tag_linc,
-                    'image': tag_img,
-                }
-            )
+        cards_list.append(
+            {
+                'title': tag_name.text.strip(),
+                'bank': tag_bank.text.strip(),
+                'link': tag_linc,
+                'image': tag_img,
+            }
+        )
+
     return cards_list
 
 
 def parser():
     html = get_html(URL)
     if html.status_code == 200:
-        # print(get_content(html.text))
-        print(len(get_content(html.text)))
+        print(get_content(html.text))
+        print("Количество карт -", len(get_content(html.text)))
     else:
         print("Нет доступа!!!")
-
-# html = get_html(URL)
-# print(get_content(html.text))
-#
-
-# get_content(get_html(URL, ''))
 
 
 parser()
