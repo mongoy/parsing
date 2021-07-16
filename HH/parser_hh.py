099d4233499f549ba4b332316377e985ea29d531
mongoy <mongoy@rambler.ru>
2021-07-16 17:01:27 +0900

    Rebuild parser_hh script: content save to file and read from file

--- HH/parser_hh.py
+++ HH/parser_hh.py
@@ -7,17 +7,20 @@ experience=doesNotMatter&schedule=remote&order_by=publication_time&search_period
 no_magic=true&L_save_area=true
 """
 import requests
-from bs4 import BeautifulSoup
-import lxml
+from bs4 import BeautifulSoup as bs
+import collections
+import time
+import random
+import json
+import csv
 
 
 HOST = 'https://chita.hh.ru/'
-# URL = 'https://chita.hh.ru/search/vacancy?area=113&clusters=true&enable_snippets=true&items_on_page=100&' \
-#       'no_magic=true&order_by=publication_time&search_period=3&text=data+engineer&' \
-#       'schedule=remote&from=cluster_schedule&showClusters=true'
+
 URL = 'https://chita.hh.ru/search/vacancy?st=searchVacancy&text=data+engineer&area=113&salary=&currency_code=RUR&' \
-      'experience=doesNotMatter&schedule=remote&order_by=publication_time&search_period=3&items_on_page=100&' \
-      'no_magic=true&L_save_area=true'
+      'experience=doesNotMatter&schedule=remote&order_by=publication_time&search_period=3&items_on_page=20&' \
+      'no_magic=true&L_save_area=true&page='
+
 HEADERS = {
     'Host': 'chita.hh.ru',
     'User-agent': 'Mozilla/5.0',
@@ -27,44 +30,120 @@ HEADERS = {
 }
 
 
-def get_html(url, params=''):
+def save_to_file(src, num_row):
+    # save page to file
+    with open(f"html\index{num_row}.html", "w", encoding='utf-8') as file:
+        file.write(src)
+
+
+def read_file(num_row):
+    # read page from file
+    with open(f"html\index{num_row}.html", "r") as file:
+        src = file.read()
+    return src
+
+
+def get_page(url, params=''):
+    req = requests.get(url, headers=HEADERS, params=params)
+    status = req.status_code
+    src = req.text
+    return status, src
+
+
+def get_pages_num(src):
     """
-    Get content from url
-    :param url:
-    :param params:
+    Number of paginator pages
     :return:
     """
-    response = requests.get(URL, headers=HEADERS, params=params)
-    return response
+    page_bs = bs(src, "html.parser")
+    try:
+        return int(page_bs.find_all("span", class_="pager-item-not-in-short-range")[-1].text.strip())
+    except:
+        # only one page
+        return 1
 
 
-def get_content(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    vacancies_list = []
-    items = soup.find_all("div", class_="vacancy-serp-item")
-    print(len(items))
-    for count, item in enumerate(items):
-        # the choice remote vacancy
-        remote = item.find("div", class_="bloko-text-tertiary").text.strip()
-        if remote is not None:
-            # Можно работать из дома
-            # print(remote)
-            vacancy_name = item.find("a", class_="bloko-link")  # the name of vacancy
-            vacancy_url = vacancy_name["href"]
-            if vacancy_url is None:
-                vacancy_url = '-'
-            vacancy_org = item.find("a", class_="bloko-link bloko-link_secondary")  # the name of organization
-            org_link = HOST + vacancy_org["href"][1:]
-            vacancy_place = item.find("span", class_="vacancy-serp-item__meta-info").text.strip()
-            print(count, vacancy_place, vacancy_name.text.strip(), vacancy_url, vacancy_org.text.strip(), org_link)
+def get_data_url(src, num):
+    # Data from one page
+    save_to_file(src, num)
+    #
+    get_data(src)
 
-    return vacancies_list
+
+def get_data_file(src, num):
+    # Data from one file
+    get_data(read_file(num))
+
+
+def get_data(src):
+    page_bs = bs(src, 'lxml')
+    tags = page_bs.find_all('div', class_='vacancy-serp-item')
+
+    for tag in tags:
+        vacancy_name = tag.find("a", class_="bloko-link")  # vacancy name
+        vacancy_compensation = tag.find("div", class_="vacancy-serp-item__sidebar").text
+        if vacancy_compensation is None:
+            vacancy_compensation = '-'
+
+        vacancy_url = vacancy_name["href"]  # link vacancy
+        if vacancy_url is None:
+            vacancy_url = '-'
+
+        vacancy_org = tag.find("a", class_="bloko-link bloko-link_secondary")  # the name of organization
+
+        org_link = HOST + vacancy_org["href"][1:]
+
+        vacancy_place = tag.find("span", class_="vacancy-serp-item__meta-info").text.strip()
+
+        vacancy_response = HOST + tag.find("div",
+                                           class_="vacancy-serp-item__controls-item "
+                                                  "vacancy-serp-item__controls-item_response"
+                                           ).find('a')['href'][1:].strip()
+
+        vacancy_time = tag.find("span", class_="vacancy-serp-item__publication-date "
+                                               "vacancy-serp-item__publication-date_short").text.strip()
+
+        print(vacancy_place + ' - ',
+              vacancy_compensation + '\n',
+              vacancy_name.text.strip() + ' - ',
+              vacancy_url.strip() + '\n',
+              vacancy_org.text.strip() + ' - ',
+              org_link + '\n',
+              'Откликнуться -',
+              vacancy_response + '\n',
+              vacancy_time + '\n'
+              )
+
+    return
+
+
+def get_data_all_url(url, num_page):
+    # get data from URL
+    for num in range(num_page):
+        html = get_page(URL, params={'page': f'{num}'})
+        get_data_url(html[1], num)
+        print(f'Готово - {num + 1}. Ждём перед загрузкой следующей страницы...')
+        time.sleep(3)
+    print(f'Готово - {num + 1} из {num_page}')
+    return
+
+
+def get_data_all_file(num_page):
+    # get data from file
+    for num in range(num_page):
+        get_data_file(num)
+
+    return
 
 
 def parser():
-    html = get_html(URL)
-    if html.status_code == 200:
-        print(get_content(html.text))
+    html = get_page(URL, params={'page': 0})
+    if html[0] == 200:
+        num_pages = get_pages_num(html[1])
+        print(f'Всего {num_pages} страниц')
+        # get data from URL one or all pages
+        # get_data_page(html[1], 0)
+        get_data_all_url(URL, num_pages)
     else:
         print("Нет доступа!!!")
 
