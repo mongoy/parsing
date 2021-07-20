cdb849d1439ab32ab847314c3c4a423e87d3c3d2
mongoy <mongoy@rambler.ru>
2021-07-20 16:26:16 +0900

    Send file json on the email

--- HH/parser_hh.py
+++ HH/parser_hh.py
@@ -8,11 +8,25 @@ no_magic=true&L_save_area=true
 """
 import requests
 from bs4 import BeautifulSoup as bs
-import collections
 import time
-import random
+import datetime
 import json
-import csv
+import os
+
+from .settings import *
+# скрытый ввод пароля
+from getpass import getpass
+# Импортируем библиотеку по работе с SMTP
+import smtplib
+# Добавляем необходимые подклассы - MIME-типы
+import mimetypes  # Импорт класса для обработки неизвестных MIME-типов,
+# базирующихся на расширении файла
+from email import encoders  # Импортируем энкодер
+from email.mime.base import MIMEBase  # Общий тип
+from email.mime.text import MIMEText  # Текст/HTML
+from email.mime.image import MIMEImage  # Изображения
+from email.mime.audio import MIMEAudio  # Аудио
+from email.mime.multipart import MIMEMultipart  # Многокомпонентный объект
 
 HOST = 'https://chita.hh.ru/'
 
@@ -28,6 +42,89 @@ HEADERS = {
     'Connection': 'keep-alive'
 }
 
+
+def processing_attachement(msg, files):
+    """
+    Функция по обработке списка, добавляемых к сообщению файлов
+    :param msg: почтовое сообщение
+    :param files: путь к файлу(ам) вложения
+    :return:
+    """
+    for f in files:
+        if os.path.isfile(f):  # Если файл существует
+            attach_file(msg, f)  # Добавляем файл к сообщению
+        elif os.path.exists(f):  # Если путь не файл и существует, значит - папка
+            dir_files = os.listdir(f)  # Получаем список файлов в папке
+            for file in dir_files:  # Перебираем все файлы и...
+                attach_file(msg, f + "/" + file)  # ...добавляем каждый файл к сообщению
+
+    return
+
+
+def attach_file(msg, filepath):
+    """
+    Функция по добавлению конкретного файла к сообщению
+    :param msg: почтовое сообщение
+    :param filepath: путь к файлу(ам) вложения
+    :return:
+    """
+    filename = os.path.basename(filepath)  # Получаем только имя файла
+    ctype, encoding = mimetypes.guess_type(filepath)  # Определяем тип файла на основе его расширения
+    if ctype is None or encoding is not None:  # Если тип файла не определяется
+        ctype = 'application/octet-stream'  # Будем использовать общий тип
+    maintype, subtype = ctype.split('/', 1)  # Получаем тип и подтип
+    if maintype == 'text':  # Если текстовый файл
+        with open(filepath) as fp:  # Открываем файл для чтения
+            file = MIMEText(fp.read(), _subtype=subtype)  # Используем тип MIMEText
+            fp.close()  # После использования файл обязательно нужно закрыть
+    elif maintype == 'image':  # Если изображение
+        with open(filepath, 'rb') as fp:
+            file = MIMEImage(fp.read(), _subtype=subtype)
+            fp.close()
+    elif maintype == 'audio':  # Если аудио
+        with open(filepath, 'rb') as fp:
+            file = MIMEAudio(fp.read(), _subtype=subtype)
+            fp.close()
+    else:  # Неизвестный тип файла
+        with open(filepath, 'rb') as fp:
+            file = MIMEBase(maintype, subtype)  # Используем общий MIME-тип
+            file.set_payload(fp.read())  # Добавляем содержимое общего типа (полезную нагрузку)
+            fp.close()
+            encoders.encode_base64(file)  # Содержимое должно кодироваться как Base64
+    file.add_header('Content-Disposition', 'attachment', filename=filename)  # Добавляем заголовки
+    msg.attach(file)  # Присоединяем файл к сообщению
+
+
+def send_email(msg_subj, msg_text, files):
+    """
+    Отправка на e-mail
+    :param msg_subj: тема сообщения
+    :param msg_text: текст сообщения
+    :param files: путь к файлу(ам) вложения
+    :return:
+    """
+    # addr_from = input('Введите e-mail отправителя:')
+    password = getpass(f'Введите пароль ({addr_from}):')  # Пароль
+    # addr_to = input('Введите e-mail получателя:')
+
+    msg = MIMEMultipart()  # Создаем сообщение
+    msg['From'] = addr_from  # Адресат
+    msg['To'] = addr_to  # Получатель
+    msg['Subject'] = msg_subj  # Тема сообщения
+
+    body = msg_text
+    msg.attach(MIMEText(body, 'plain'))  # Добавляем в сообщение текст
+    # присоединение файла
+    processing_attachement(msg, files)
+
+    server = smtplib.SMTP('smtp.mail.ru', 25)  # Создаем объект SMTP для MAIL.RU
+    # server.set_debuglevel(True)  # Включаем режим отладки - если отчет не нужен, строку можно закомментировать
+    server.starttls()  # Начинаем шифрованный обмен по TLS
+    server.login(addr_from, password)  # Получаем доступ
+    server.send_message(msg)  # Отправляем сообщение
+    server.quit()  # Выходим
+
+    return
 
 
 def save_to_file(src, num_row):
@@ -117,18 +214,11 @@ def get_pages_num(src):
         return 1
 
 
-def get_data_url(src, num):
-    # Data from one page
-    save_to_file(src, num)
-    #
-    get_data(src)
-
-
 def get_data_all_url(url, num_page):
     # get data from URL
     for num in range(num_page):
         html = get_page(URL, params={'page': f'{num}'})
-        get_data_url(html[1], num)
+        save_to_file(html[1], num)
         print(f'Готово - {num + 1}. Ждём перед загрузкой следующей страницы...')
         time.sleep(3)
     print(f'Готово - {num + 1} из {num_page}')
@@ -148,22 +238,33 @@ def get_data_all_file(num_page):
     return
 
 
+def send_sub():
+    """
+    Отправка почтового сообщения с почтового сервера MAIL.RU
+    :return:
+    """
+    files = [r"HH\all_vacancy_dict.json"]
+    now = datetime.datetime.now()
+    msg_subj = "Вакансии с HH.ru"
+    msg_text = "HH. Список вакансий на " + str(now.strftime("%d-%m-%Y %H:%M"))
+    send_email(msg_subj, msg_text, files)
+    return
+
+
 def parser():
-    # html = get_page(URL, params={'page': 0})
-    # if html[0] == 200:
-    #     num_pages = get_pages_num(html[1])
-    #     print(f'Всего {num_pages} страниц')
-    #     # get data from URL one or all pages
-    #     # get_data_page(html[1], 0)
-    #     # get_data_all_url(URL, num_pages)
-    # else:
-    #     print("Нет доступа!!!")
-
-    ###
-
-    html = read_file(0)
-    num_pages = get_pages_num(html)
-    get_data_all_file(num_pages)
+    html = get_page(URL, params={'page': 0})
+    if html[0] == 200:
+        num_pages = get_pages_num(html[1])
+        print(f'Всего {num_pages} страниц')
+        # get data from URL one or all pages
+        get_data_all_url(URL, num_pages)
+        # parsing saved html file
+        html = read_file(0)
+        get_data_all_file(num_pages)
+        send_sub()
+    else:
+        print("Нет доступа!!!")
 
 
-parser()
+# parser()
+send_sub()
---
+++ HH/settings.py
@@ -0,0 +1,2 @@
+addr_from = "a_antropov@mail.ru"     # Адресат
+addr_to = "a_antropov@mail.ru"  # Получатель
