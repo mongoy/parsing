ebe2994da9dc08a518e6b192a22437a362cf39df
mongoy <mongoy@rambler.ru>
2021-07-25 01:20:19 +0900

    Finish commit script API YoBit

--- YoBit/main.py
+++ YoBit/main.py
@@ -28,16 +28,44 @@ def get_ticker(coin1='btc', coin2='usd'):
 
 def get_depth(coin1='btc', coin2='usd', limit=150):
     # get info about  cryptocurrency pairs eth_btc)
-    response = requests.get(url=f"{URL}depth/{coin1}_{coin2}?{limit}=150&ignore_invalid=1")
+    response = requests.get(url=f"{URL}depth/{coin1}_{coin2}?limit={limit}&ignore_invalid=1")
     with open("depth.txt", "w") as file:
         file.write(response.text)
-    return response.text
+
+    bids = response.json()[f"{coin1}_{coin2}"]["bids"]
+    total_bids_amount = 0
+    for item in bids:
+        price = item[0]
+        coin_amount = item[1]
+        total_bids_amount += coin_amount * price
+
+    return f"Total bids:{total_bids_amount} $"
+
+
+def get_trades(coin1='btc', coin2='usd', limit=150):
+    # get info about cryptocurrency pairs eth_btc and xrp_btc)
+    response = requests.get(url=f"{URL}trades/{coin1}_{coin2}?limit={limit}&ignore_invalid=1")
+    with open("trades.txt", "w") as file:
+        file.write(response.text)
+
+    total_trades_ask = 0
+    total_trades_bid = 0
+    for item in response.json()[f"{coin1}_{coin2}"]:
+        if item["type"] == 'ask':
+            total_trades_ask += item["price"] * item["amount"]
+        else:
+            total_trades_bid += item["price"] * item["amount"]
+    info = f"[-] Total {coin1} Sell:{round(total_trades_ask, 2)} $ \n" \
+           f"[+] Total {coin1} Sell:{round(total_trades_bid, 2)} $"
+    return info
 
 
 def parser():
     # print(get_info())
     # print(get_ticker())
-    print(get_depth())
+    # print(get_depth(limit=150))
+    # print(get_depth(limit=2000))
+    print(get_trades(limit=150))
 
 
 if __name__ == "__main__":
